29ace796dc91a72ae0f6ba5ead9ad443022a7a3d
Roshan <rmp99036@gmail.com>
2024-03-26 17:37:13 +0530

    xyz

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,17 +1,17 @@
 "use client"
 import React, { useState } from "react";
-import { Box, Flex, Heading, Input, Button, FormControl, FormLabel, useToast, Link } from "@chakra-ui/react";
+import { Box, Flex, Heading, Input, Button, FormControl, FormLabel, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [loading, setLoading] = useState(false);
-  const toast = useToast();
-  const router = useRouter();
+  const [email, setemail] = useState()
+  const [password, setpassword] = useState()
+  const [loading,setLoading] = useState(false)
+  const toast = useToast()
+  const router = useRouter()
 
   const handleClick = async () => {
-    setLoading(true);
+    setLoading(true)
     if (!email || !password) {
       toast({
         title: 'Warning',
@@ -19,8 +19,8 @@ const Page = () => {
         status: 'error',
         duration: 4000,
         isClosable: true,
-      });
-      setLoading(false);
+      })
+      setLoading(false)
       return;
     }
 
@@ -33,22 +33,22 @@ const Page = () => {
         email,
         password,
       }),
-    });
+    })
 
-    const data = await response.json();
-    if(data.status === "error"){
-      alert("Wrong Credentials!");
-      setLoading(false);
-      return;
+    const data = await response.json()
+    if(data.status == "error"){
+      alert("wrong Credentials!")
+      setLoading(false)
+      return
     }
     else{
-      const token = data.user;
-      localStorage.setItem("jwttoken", token);
-      router.push("/");
+      const token = data.user
+      localStorage.setItem("jwttoken",token)
+      router.push("/")
     }
-    setLoading(false);
-  };
+    setLoading(false)
 
+  }
   return (
     <Flex
       minH="100vh"
@@ -70,7 +70,7 @@ const Page = () => {
         <Box my={4} textAlign="left">
           <FormControl>
             <FormLabel>Email:</FormLabel>
-            <Input placeholder="Username" size="lg" onChange={(e) => setEmail(e.target.value)} />
+            <Input placeholder="Username" size="lg" onChange={(e) => setemail(e.target.value)} />
           </FormControl>
           <FormControl mt={6}>
             <FormLabel>Password:</FormLabel>
@@ -78,15 +78,12 @@ const Page = () => {
               placeholder="Password"
               size="lg"
               type="password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setpassword(e.target.value)}
             />
           </FormControl>
           <Button isLoading={loading} onClick={handleClick} colorScheme="teal" size="lg" mt={8} w="100%">
             Sign In
           </Button>
-          <Box mt={4} textAlign="center">
-            <Link href="/register" color="teal.500">Don't have an account? Register</Link>
-          </Box>
         </Box>
       </Box>
     </Flex>
